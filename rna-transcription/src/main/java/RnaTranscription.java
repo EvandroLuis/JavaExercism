c4fc0da9da5d022fde34eba98cb355d8b5a98f34
Evandro Luis <evandroluis58@gmail.com>
2018-02-11 21:41:08 -0300

    Dna to rna

---
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class RnaTranscription {
+
+    String transcribe(String dnaStrand) {
+        String rnaStrand="";
+        for (int i = 0; i<dnaStrand.length(); i++) {
+
+            switch (dnaStrand.charAt(i)) {
+                case 'G': rnaStrand += "C";
+                    break;
+                case 'C': rnaStrand += "G";
+                    break;
+                case 'T': rnaStrand += "A";
+                    break;
+                case 'A': rnaStrand += "U";
+                    break;
+            }
+        }
+        return rnaStrand;
+    }
+
+}
